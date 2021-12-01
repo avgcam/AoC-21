ca2b2f563a7c8ba7e032468e7f4802026e7dae2c
avgcam <meadowsc1@udayton.edu>
2021-12-01 14:58:22 -0500

    first commit

--- solution1.py
+++ solution1.py
@@ -1,7 +1,45 @@
-import numpy as np
+def readLines():
+    numbers = []
 
-file = 'input1.txt'
-data = np.loadtxt(file, delimiter=' ', dtype=int)
+    with open('input1.txt') as f:
+        for line in f.readlines():
+            numbers.append(int(line))
 
-for i in data:
-	print(i)
+    return numbers
+
+
+def s1():
+    report = readLines()
+    previous = None
+    increases = 0
+
+    for depth in report:
+        if previous != None and depth > previous:
+            increases += 1
+        previous = depth
+
+    print('Solution 1:', increases)
+
+
+def s2():
+    report = readLines()
+    previous = None
+    increases = 0
+
+    threes = list(zip(report, report[1:], report[2:]))
+    for (a, b, c) in threes:
+        total = a + b + c
+        if previous != None and total > previous:
+            increases += 1
+        previous = total
+
+    print('Solution 2:', increases)
+
+
+def main():
+    s1()
+    s2()
+
+
+if __name__ == '__main__':
+    main()
