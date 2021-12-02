a3f37c2a9bdc3ce9d64234ab003dc7dd9145c184
avgcam <meadowsc1@udayton.edu>
2021-12-02 09:51:01 -0500

    day2

---
+++ day2/solution2.py
@@ -0,0 +1,45 @@
+def readlines():
+	x = []
+	with open('input2.txt') as file:
+		for line in file:
+			dir, n = line.rstrip().split(" ")
+			n = int(n)
+			x.append((dir, n))
+		x = tuple(x)
+	return x
+
+def part1(x):
+	depth = 0
+	hor_pos = 0
+
+	for (dir, n) in x:
+		if dir == 'forward':
+			hor_pos += n
+		elif dir == 'up':
+			depth -= n
+		elif dir == 'down':
+			depth += n
+	return hor_pos*depth
+
+def part2(x):
+	depth = 0
+	hor_pos = 0
+	aim = 0
+
+	for (dir, n) in x:
+		if dir == 'forward':
+			hor_pos += n
+			depth += aim * n
+		elif dir == 'up':
+			aim -= n
+		elif dir == 'down':
+			aim += n
+	return hor_pos*depth
+
+def main():
+    print(part1(readlines()))
+    print(part2(readlines()))
+
+
+if __name__ == '__main__':
+	main()
