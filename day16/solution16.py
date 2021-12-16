75bbff60befe934275afb2c05f004e757dd94e79
avgcam <meadowsc1@udayton.edu>
2021-12-16 08:02:02 -0500

    day16 template

---
+++ day16/solution16.py
@@ -0,0 +1,16 @@
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+def load_data():
+	None
+
+def part1():
+	None
+
+def part2():
+	None
+
+if __name__ == "__main__":
+	part1()
+	part2()
